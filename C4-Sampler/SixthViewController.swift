08004691e77a7298762613c4cbd0414c101a7c2b
hahahahahanananana <hkataoka925@st1.shinagawajoshi.ed.jp>
2017-10-29 22:46:08 +0900

    20171029

--- C4-Sampler/SixthViewController.swift
+++ C4-Sampler/SixthViewController.swift
@@ -12,47 +12,48 @@ class SixthViewController: CanvasController {
     var timer: Timer!
     var gradients = [Gradient]()
     
-    
     override func setup() {
-        let g = createGradient(point: canvas.center)
-        createAnimation(g: g)
+        //        let g =  createGradient(canvas.center)
+        //        self.createAnim(g)
         var x = 2.0
+        repeat {
+            gradients.append(createGradient(point: Point(x,canvas.center.y)))
+            x += 3.0
+        } while x < canvas.width
+        
+        
         timer = Timer(interval: 0.02, count:gradients.count ){ () -> () in
             let g = self.gradients[self.timer.step]
             self.createAnimation(g: g)
         }
         timer.start()
-        repeat {
-            gradients.append(createGradient(point: x,canvas.center.y))
-            x += 3.0
-        } while x < anvas.width
-        
-        func createGradient(point: Point) -> Gradient
-        var colors = [C4Blue,C4Pink]
-        
-        if random(below: 2) == 1 {
-            colors = [C4Pink,C4Blue]
-        }
-        
-        let g = Gradient(frame: Rect(0,0,2,2))
-        g.colors = colors
-        g.center = point
-        canvas.add(g)
-        return g
+    }
+    func createGradient(point: Point) -> Gradient {
+    var colors = [C4Blue,C4Pink]
+    
+    if random(below: 2) == 1 {
+    colors = [C4Pink,C4Blue]
     }
     
-    func createAnimation(g: Gradient){
-        let anim = ViewAnimation(duration: 2.0){
-            var f = g.frame
-            let c = g.center
-            f.height = 100
-            f.center = c
-            g.frame = f
-        }
-        anim.curve = .EaseInOut
-        anim.autoreverses = true
-        anim.repeats = true
-        anim.animate()
+    let g = Gradient(frame: Rect(0,0,2,2))
+    g.colors = colors
+    g.center = point
+    canvas.add(g)
+    return g
+}
+
+func createAnimation(g: Gradient){
+    let anim = ViewAnimation(duration: 2.0){
+        var f = g.frame
+        let c = g.center
+        f.height = 100
+        f.center = c
+        g.frame = f
     }
+    anim.curve = .EaseInOut
+    anim.autoreverses = true
+    anim.repeats = true
+    anim.animate()
+}
 }
 
